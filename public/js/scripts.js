b3855f1ad8b47351a31cedb9e3bbc36fa913c37c
Andrew James <ajames20@gmail.com>
2018-08-14 13:37:44 -0600

    Add locking funcitonality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,6 @@
 const colorBlockList = document.querySelectorAll('.color-blocks__block');
+const colorBlockSection = document.querySelector('.color-blocks');
+const openLockList = document.querySelectorAll('.fa-lock-open');
 const randomButton = document.querySelector('.controls-section__button');
 const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`; // 16777215 = #ffffff;
 
@@ -7,5 +9,11 @@ const setColorPallet = () => {
     block.setAttribute('style', `--color-${index + 1}: ${getRandomHexColor()}`);
   });
 };
+
+colorBlockSection.addEventListener('click', event => {
+  if (event.target.tagName === 'I') {
+    event.target.classList.toggle('fa-lock-open');
+  }
+});
 
 randomButton.addEventListener('click', setColorPallet);
