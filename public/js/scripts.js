cfb1503886cc865f9f1578f5227fb91496444906
Andrew James <ajames20@gmail.com>
2018-08-14 12:09:19 -0600

    Add initial set up for fetching pallet

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,8 @@
-console.log('found it');
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`; // 16777215 = #ffffff;
+const setColorPallet = async () => {
+  const response = await fetch('/api/v1/pallet');
+  const pallet = await response.json();
+  console.log(pallet);
+};
+setColorPallet();
